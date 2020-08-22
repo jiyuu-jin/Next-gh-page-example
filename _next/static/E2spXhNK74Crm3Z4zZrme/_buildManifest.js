2675c8ddeced9100bc7bbc0882c3fd4f3fa02b1b
Zachary Pelkey <zpelkey@gmail.com>
2020-08-22 11:29:52 -0400

    Updates

---
+++ _next/static/E2spXhNK74Crm3Z4zZrme/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-1fcc75f12c181292f038.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ff3673b4e910f9435a1e.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout-17b9c34f275d944255d6.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
